9294d6e08e6dc07adabe60805d140cb132ea6a04
Utku Berki Baysal <utkuberki21@gmail.com>
2025-04-11 09:07:49 +0300

    Add project files.

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace RecapDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new SmsLogger();
+        }
+    }
+
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+            Logger.Log();
+            Console.WriteLine("Customer added!");
+        }
+    }
+
+    class DatabaseLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to database! ");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to file! ");
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Sms! ");
+        }
+    }
+
+    interface ILogger
+    {
+        void Log();
+    }
+}
